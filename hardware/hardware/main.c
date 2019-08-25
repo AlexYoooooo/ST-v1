892676dde34100dc1612bc04ad6926119a75529a
Captdam <captdam@beardle.com>
2019-08-25 05:48:02 -0400

    Finish code for hardware, NOT TESTED!

--- hardware/hardware/main.c
+++ hardware/hardware/main.c
@@ -23,7 +23,6 @@ volatile uint8_t query[1024*4]; //1024 instruction word in total, 4-byte per wor
 volatile struct Phasor axisW, axisX, axisY, axisZ;
 
 
-
 /************************************************************************/
 /* ROM consts, lookup tables                                            */
 /************************************************************************/
@@ -47,11 +46,13 @@ uint8_t requestSerialSync() {
 	return UDR0;
 }
 
+//External packed routine
+
+#include "../../command phasor/phasor-eventdriven.c"
+#include "motor.c"
+
 
 // MAIN ROUTINES ---------------------------------------------------------
-
-#include "phasor.c"
-#include "motor.c"
 
 //Init hardwares
 int main(void) {
@@ -89,12 +90,94 @@ int main(void) {
 	TCCR5B = 0b00001000;
 	TIMSK5 = 0b00000010;
 	
-	
-	
-	//System ready
+	//System ready, listen to front-end
 	sei();
-	for(;;) {
+	uint8_t checksum;
+	uint8_t receive;
+	sendSerialSync(0x6D); //System OK = 0b01101101 <-- Use this pattern to check BAUD paring
+	
+	for(;;) { //System main loop
+		receive = requestSerialSync();
 		
+		if (receive == 0x00) { //Send instruction query to MCU
+			checksum = 0;
+			for (uint16_t i = 0; i < 4096; i++) {
+				receive = requestSerialSync();
+				query[i] = receive;
+				checksum -= receive;
+			}
+			
+			phasorReset(&axisW, (query[4096-4*2]<<8) | query[4096-4*2+1] ); //query[4088-4089]
+			phasorReset(&axisX, (query[4096-3*2]<<8) | query[4096-3*2+1] ); //query[4090-4091]
+			phasorReset(&axisY, (query[4096-2*2]<<8) | query[4096-2*2+1] ); //query[4092-4093]
+			phasorReset(&axisZ, (query[4096-1*2]<<8) | query[4096-1*2+1] ); //query[4094-4095]
+			
+			sendSerialSync(checksum); //Return checksum
+		}
+		
+		else { //Instant command
+			switch ( (receive&0b11000000) >> 6 ) { //W-axis command
+				case 3: //Start/stop: 11
+					if (getW())
+						stopW();
+					else
+						startW();
+					break;
+				case 2: //Forward step: 10
+					stepW(1);
+					break;
+				case 1: //Backward step: 01
+					stepW(0);
+					break;
+				/* case 0: //No action: 00 */
+			}
+	
+			switch ( (receive&0b00110000) >> 4 ) { //X-axis command
+				case 3:
+					if (getX())
+						stopX();
+					else
+						startX();
+					break;
+				case 2:
+					stepX(1);
+					break;
+				case 1:
+					stepX(0);
+					break;
+			}
+	
+			switch ( (receive&0b00001100) >> 2 ) { //Y-axis command
+				case 3:
+					if (getY())
+						stopY();
+					else
+						startY();
+					break;
+				case 2:
+					stepY(1);
+					break;
+				case 1:
+					stepY(0);
+					break;
+			}
+		
+			switch (receive&0b00000011) { //Y-axis command
+				case 3:
+					if (getZ())
+						stopZ();
+					else
+						startZ();
+					break;
+				case 2:
+					stepZ(1);
+					break;
+				case 1:
+					stepZ(0);
+					break;
+			}
+			sendSerialSync(receive); //Return cmd to ACK the cmd
+		}
 		
 	}
 	
@@ -103,6 +186,30 @@ int main(void) {
 
 
 // ISR Interrupt Service Routine -----------------------------------------
+
+ISR(TIMER1_COMPA_vect) { //Time delay reached
+	uint16_t phasorResult = phase(&axisW); //Phasor process instruction query
+	setW(phasorResult & 0x7FFF); //Set delay (motor speed)
+	stepW(phasorResult >> 15); //Step motor forward/backward
+}
+
+ISR(TIMER3_COMPA_vect) {
+	uint16_t phasorResult = phase(&axisX);
+	setX(phasorResult & 0x7FFF);
+	stepX(phasorResult >> 15);
+}
+
+ISR(TIMER4_COMPA_vect) {
+	uint16_t phasorResult = phase(&axisY);
+	setY(phasorResult & 0x7FFF);
+	stepY(phasorResult >> 15);
+}
+
+ISR(TIMER5_COMPA_vect) {
+	uint16_t phasorResult = phase(&axisZ);
+	setZ(phasorResult & 0x7FFF);
+	stepZ(phasorResult >> 15);
+}
 
 
 /************************************************************************/
--- hardware/hardware/motor.c
+++ hardware/hardware/motor.c
@@ -1,4 +1,6 @@
-//Axis-W Timer0
+#include <util/delay_basic.h>
+
+//Axis-W Timer1
 void startW() { //Timer scaler <- 1
 	TCCR1B |= 0b00000001;
 }
@@ -7,7 +9,21 @@ void stopW() { //Timer scaler <- 0
 }
 void setW(uint16_t delay) { //Set compare value
 	OCR1A = delay;
-	
+}
+uint8_t getW() {
+	return TCCR1B & 0b00000111;
+}
+void stepW(uint8_t dir) { //Motor step forward/backward
+	cli(); //STEP = D.7, DIR = D.6
+	if (dir)
+		PORTD |= 0b01000000;
+	else
+		PORTD &= 0b10111111;
+	_delay_loop_1(3); //9 CPU cycle = 9/16us, A5958 requires 400ns setup time
+	PORTD |= 0b10000000; //STEP rise
+	_delay_loop_1(5); //15 CPU cycle = 15/16us, plus the PORTD |= 0b10000000 get 1us. A5958 requires 1us pulse time
+	PORTD &= 0b01111111; //STEP fall
+	sei();
 }
 
 //Axis-X Timer3
@@ -20,6 +36,21 @@ void stopX() {
 void setX(uint16_t delay) {
 	OCR3A = delay;
 }
+uint8_t getX() {
+	return TCCR3B & 0b00000111;
+}
+void stepX(uint8_t dir) {
+	cli(); //STEP = C.0, DIR = G.1
+	if (dir)
+		PORTG |= 0b00000010;
+	else
+		PORTG &= 0b11111101;
+	_delay_loop_1(3);
+	PORTC |= 0b00000001;
+	_delay_loop_1(5);
+	PORTC &= 0b11111110;
+	sei();
+}
 
 //Axis-Y Timer4
 void startY() {
@@ -31,6 +62,21 @@ void stopY() {
 void setY(uint16_t delay) {
 	OCR4A = delay;
 }
+uint8_t getY() {
+	return TCCR4B & 0b00000111;
+}
+void stepY(uint8_t dir) {
+	cli(); //STEP = C.7, DIR = C.5
+	if (dir)
+		PORTC |= 0b00100000;
+	else
+		PORTC &= 0b11011111;
+	_delay_loop_1(3);
+	PORTC |= 0b10000000;
+	_delay_loop_1(5);
+	PORTC &= 0b01111111;
+	sei();
+}
 
 //Axis-Z Timer5
 void startZ() {
@@ -41,4 +87,19 @@ void stopZ() {
 }
 void setZ(uint16_t delay) {
 	OCR5A = delay;
+}
+uint8_t getZ() {
+	return TCCR5B & 0b00000111;
+}
+void stepZ(uint8_t dir) {
+	cli(); //STEP = L.7, DIR = L.6
+	if (dir)
+		PORTL |= 0b01000000;
+	else
+		PORTL &= 0b10111111;
+	_delay_loop_1(3);
+	PORTL |= 0b10000000;
+	_delay_loop_1(5);
+	PORTL &= 0b01111111;
+	sei();
 }
