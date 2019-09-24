662852614a28ee0d90da207815b55752634fd15a
AlexYoooooo <47153128+AlexYoooooo@users.noreply.github.com>
2019-09-24 01:17:06 -0400

    use serial read as delay

--- testoutput.m
+++ testoutput.m
@@ -2,7 +2,7 @@ a=input('Enter instruction file name :','s');
 
 pkg load instrument-control
 s1 = serial("/dev/ttyUSB0");
-set(s1,'baudrate',600);
+set(s1,'baudrate',4800);
 set(s1, 'bytesize',8);
 set(s1,'parity','n');
 set(s1,'stopbits',2);
@@ -31,57 +31,17 @@ srl_write(s1,uint8(0));
 c=1;
 for ln = 1:1024
 	for col = 1:4
-		A(ln,col)
+		
 		c=c+1
 		srl_write(s1,uint8(A(ln,col)));
+
 	end
 end
+opt=uint8(srl_read(s1,10000))
 
-	
-
-c=1
-opt=uint8(srl_read(s1,100))
-s= size(opt)
-s=s(2)
-while s != 1
-srl_write(s1,uint8(255));
-c=c+1
-opt=uint8(srl_read(s1,100))
-s= size(opt)
-s=s(2)
-
-end
 
 
 srl_write(s1,uint8(255));
 x=input('done, press ENTER to exit');
 
 opt=uint8(srl_read(s1,10))
-
-
-		end
-		opt=uint8(srl_read(s1,10));
-
-	else
-	break
-	end
-end
-c=1
-opt=uint8(srl_read(s1,100))
-s= size(opt)
-s=s(2)
-while s != 1
-srl_write(s1,uint8(255));
-c=c+1
-opt=uint8(srl_read(s1,100))
-s= size(opt)
-s=s(2)
-
-end
-
-
-srl_write(s1,uint8(255));
-x=input('done, press ENTER to exit');
-
-opt=uint8(srl_read(s1,10))
-
