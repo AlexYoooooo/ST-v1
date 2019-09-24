2fae5e4d9e08bdb59f424c3a9485813675764b44
AlexYoooooo <47153128+AlexYoooooo@users.noreply.github.com>
2019-09-24 01:15:39 -0400

    fix motar instruction start location

--- compiler.m
+++ compiler.m
@@ -10,21 +10,21 @@ mp(4)=input('Enter pitch of motor 4 in mm/rev :');
 %step	mod	spd/rep/rps	dir	time/delay
 %4	1	16		1	15	
 fmt=('%04f %01f %05f %01f %05f');
-fmt2=('%04x%04x');
+fmt2=('%04x%04x\n');
 
 A=fscanf(ipt,fmt,[5 Inf]);
-B=uint16(zeros(2,2048));
+B=uint16(zeros(2,1024));
 m=1;
 sz=size(A);
 sz=uint16(sz(2));
 o=zeros(2,sz,'uint16');
 mn=1;
 ms=zeros(1,5);
-ms(1)=1;
+ms(1)=0;
 for m = 1:sz
-		if A(3,m)>65534.9 
+		if A(3,m)>65534.9 && A(4,m)<0.1
 			mn=mn+1;
-			ms(mn)=m+1;
+			ms(mn)=m;
 		end
 		if A(2,sz)-1<0.01;
 			B(1,m)=A(3,m)-1;
@@ -32,11 +32,31 @@ for m = 1:sz
 	
 		elseif A(2,sz)-2<0.01;
 			B(1,m)=A(3,m)*A(5,m)*1792-1;
-			B(2,m)=32768*A(4,m)+1/A(3,m)/1792*1000;
+			B(2,m)=32768*A(4,m)+1/A(3,m)/1792*1000000;
 
 		else
 			B(1,m)=A(3,m)*A(5,m)*1792/mp(mn)-1;
-			B(2,m)=32768*A(4,m)+1/A(3,m)*mp(mn)/1792*1000;
+			B(2,m)=32768*A(4,m)+1/A(3,m)*mp(mn)/1792*1000000;
+		
+		end
+
+
+	
+end
+	ms(mn)=0;
+
+	B(1,1023)=ms(1);
+	B(2,1023)=ms(2);
+	B(1,1024)=ms(3);
+	B(2,1024)=ms(4);
+
+
+dec2hex(B);
+fprintf(opt,fmt2,B);
+x=input('done, press ENTER to exit');
+
+
+
 		
 		end
 
